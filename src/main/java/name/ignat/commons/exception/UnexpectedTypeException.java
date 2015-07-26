faa3a7b9a142d6bd5153344f9855a4e3ac0e3866
Dan Ignat <5143789+dan-ignat@users.noreply.github.com>
2015-07-25 21:52:43 -0500

    Added UnexpectedException and subclasses

---
+++ src/main/java/name/ignat/commons/exception/UnexpectedTypeException.java
@@ -0,0 +1,39 @@
+package name.ignat.commons.exception;
+
+/**
+ * Signifies that an unexpected type (class) has been encountered in a conditional statement.  Should be used mainly in
+ * {@code else} blocks to show the unexpected {@code value.getClass()} that was encountered.
+ * <p />
+ * Example:
+ * <p />
+ * <pre>
+ * if (shape instanceof Circle)
+ * {
+ *     ...
+ * }
+ * else if (shape instanceof Rectangle)
+ * {
+ *     ...
+ * }
+ * else
+ * {
+ *     throw new UnexpectedTypeException(shape);
+ * }
+ * </pre>
+ * <p />
+ * Good object-oriented design should minimize the need for such type-based conditional statements, but once in a while
+ * they are necessary.
+ * <p />
+ * This pattern makes your conditional statements <i>fail-fast</i> if the universe of values grows in the future and you
+ * forget to update all of your code to handle the new values.
+ * 
+ * @author Dan Ignat
+ */
+@SuppressWarnings("serial")
+public class UnexpectedTypeException extends UnexpectedException
+{
+	public UnexpectedTypeException(Object value)
+	{
+		super("Unexpected type: " + value.getClass());
+	}
+}
