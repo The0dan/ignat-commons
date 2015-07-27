a69876cbbbbe84e07021306d9303c2fa79b0b5be
Dan Ignat <5143789+dan-ignat@users.noreply.github.com>
2015-07-26 21:03:25 -0500

    Removed two methods of questionable utility from IoUtils

--- src/main/java/name/ignat/commons/utils/IoUtils.java
+++ src/main/java/name/ignat/commons/utils/IoUtils.java
@@ -1,12 +1,9 @@
 package name.ignat.commons.utils;
 
-import java.io.ByteArrayInputStream;
-import java.io.Closeable;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.io.IOUtils;
 import org.springframework.core.io.ClassPathResource;
 
@@ -67,41 +64,6 @@ public final class IoUtils
 		catch (IOException e)
 		{
 			throw new UnexpectedException(e);
-		}
-	}
-
-	/**
-	 * Decodes a Base64 string and wraps its decoded contents in a {@link ByteArrayInputStream}.
-	 * <p>
-	 * <em>NOTE:</em> Returns {@link ByteArrayInputStream} instead of {@link InputStream} so that clients are aware that
-	 * no resource leaks will result if they don't call {@link ByteArrayInputStream#close()}.
-	 * 
-	 * @author Dan Ignat
-	 */
-	public static ByteArrayInputStream decodeBase64ToInputStream(String base64Content)
-	{
-		byte[] base64Bytes = Base64.decodeBase64(base64Content);
-
-		return new ByteArrayInputStream(base64Bytes);
-	}
-
-	/**
-	 * Similar to {@link IOUtils#closeQuietly(Closeable)}, but wraps any {@link IOException} instead of swallowing it.
-	 * 
-	 * @author Dan Ignat
-	 */
-	public static void closeGracefully(Closeable closeable)
-	{
-		if (closeable != null)
-		{
-			try
-			{
-				closeable.close();
-			}
-			catch (IOException e)
-			{
-				throw new UnexpectedException(e);
-			}
 		}
 	}
 
