025a73d5ea00b3aa87214a6dae90b1020fa6fdc0
Dan Ignat <5143789+dan-ignat@users.noreply.github.com>
2020-04-26 17:33:50 -0500

    Added package javadoc

---
+++ src/main/java/name/ignat/commons/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Common code that I've found myself reusing many times over the years.
+ * <p>
+ * Notes:
+ * <ul>
+ * <li>{@code Cautiously} in a method name means it doesn't wrap any exceptions with {@code UnexpectedException}.</li>
+ * </ul>
+ * 
+ * @author Dan Ignat
+ */
+package name.ignat.commons;
